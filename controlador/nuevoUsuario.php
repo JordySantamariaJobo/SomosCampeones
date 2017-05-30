860f85ffaf0ceece76e0f67fd3c212c125c7c736
JordySantamariaJobo <jordyoctopus24@gmail.com>
2017-05-30 17:47:46 -0500

    Envio de Resultado (Propuesta)

--- controlador/nuevoUsuario.php
+++ controlador/nuevoUsuario.php
@@ -5,36 +5,49 @@
 
 	$modelo = new Usuario;
 
-    $apodo = $_REQUEST['apodo'];
-    $correo = $_REQUEST['correo'];
-    $pass = $_REQUEST['pwd'];
-    $fecha = $_REQUEST['fecha'];
-    $equipo = $_REQUEST['equipo'];
-    $codigo = $_REQUEST['cod_inv'];
-    $nombre = null;
-    $app = null;
+    	$apodo = $_REQUEST['apodo'];
+    	$correo = $_REQUEST['correo'];
+    	$pass = $_REQUEST['pwd'];
+    	$fecha = $_REQUEST['fecha'];
+    	$equipo = $_REQUEST['equipo'];
+    	$codigo = $_REQUEST['cod_inv'];
+    	$nombre = null;
+    	$app = null;
 
-    $resApodo = $modelo->VerificarApodo($apodo);
-    $verif = $modelo->getVerificacionCorreo($correo);
+    	$resApodo = $modelo->VerificarApodo($apodo);
+    	$verif = $modelo->getVerificacionCorreo($correo);
 
-    if($resApodo == 0 && $verif == 0){
-    	$modelo->setNuevoUsuario($nombre, $app, $apodo, $correo, $pass, $fecha, $equipo);
+    	if($resApodo == 0 && $verif == 0){
+    		$modelo->setNuevoUsuario($nombre, $app, $apodo, $correo, $pass, $fecha, $equipo);
    		$id = $modelo->getDatosUsuario($correo);
-    	$modelo->setRegistrarHistorial($id);
+    		$modelo->setRegistrarHistorial($id);
     
-    	if(isset($codigo)) {
-    		$res = $modelo->setVerificarCodigoInvitacion($codigo);
-    		if (isset($res['puntos'])) {
-    			$puntos = $res['puntos'] + 2500;
-    			$modelo->getRegaliaCodigoInvitacion($res['id_usuario'], $puntos);
-    			$codigo = "Codigo Valido";
-    		} else {
-    			$codigo = "Codigo Invalido";
+    		if(isset($codigo)) {
+    			$res = $modelo->setVerificarCodigoInvitacion($codigo);
+    			if (isset($res['puntos'])) {
+    				$puntos = $res['puntos'] + 2500;
+    				$modelo->getRegaliaCodigoInvitacion($res['id_usuario'], $puntos);
+    				$codigo = "Valido";
+    			} else {
+    				$codigo = "Invalido";
+    			}
     		}
+
+    		$registro = "Correcto";
+    	} else {
+    		$registro = "Incorrecto";
+		
+		if($resApodo > 0){ $apodo = 0; }
+		else { $apodo = 1; }
+		
+		if($verif > 0) { $correo = 0; }
+		else { $correo = 1; }
     	}
 
-    	$registro = "Registro Correcto";
-    } else {
-    	$registro = "Registro Incorrecto";
-    }
+	echo [
+		'registro' => $registro,
+		'codigo' => $codigo,
+		'apodo' => $apodo,
+		'correo' => $correo
+	];
 ?>
